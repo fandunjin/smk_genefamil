300f6376270030b3d84f3dbfd66c1248c39e9cd4
DunJin-Fan <fandunjin@foxmail.com>
2025-08-26 11:16:27 +0800

    Add files via upload

---
+++ hpc/smk.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#JSUB -J smk
+#JSUB -n 48
+#JSUB -q normal
+#JSUB -o smk_log.%J
+#JSUB -e smk_err.%J
+#JSUB -cwd smk_%J
+source /hpcfile/users/92024269/cx/software/miniconda3/etc/profile.d/conda.sh
+conda activate genefamily
+
+conda env list > condainfo
+
+cd /hpcfile/users/92024269/fdj/smk_genefamily/
+
+snakemake -s /hpcfile/users/92024269/fdj/smk_genefamily/snakefile --core 48  combine_all_gene_pep_info
+snakemake -s /hpcfile/users/92024269/fdj/smk_genefamily/snakefile --core 48  plot_all_trees_single
+snakemake -s /hpcfile/users/92024269/fdj/smk_genefamily/snakefile --core 48  plot_all_trees_multi
+snakemake -s /hpcfile/users/92024269/fdj/smk_genefamily/snakefile --core 48  run_all_meme
